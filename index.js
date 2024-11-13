56c82d909427202eb7064f693b73c13e9b058961
Lindanny9721 <32419597+Lindanny9721@users.noreply.github.com>
2024-11-13 14:04:59 -0500

    Finish part 1

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ const app = express();
 const PORT = 3000;
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Home Page' });
+});
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About Us' });
+});
+app.get('/user/:userID', (req, res) => {
+    res.render('user', { title: 'User Profile', user: req.params.userID });
+});
+app.post('/submit', (req, res) => {
+    console.log('Form:', req.body);
+    res.send('Form submitted');
+});
+app.listen(PORT, () => {
+    console.log('Server is running on port 3000');
+});
